6521d72c5a0a8f7f49c53ef1ae14c9abc8c09ae2
dark8605 <3389161@qq.com>
2018-11-30 14:39:55 +0800

    log日志模块添加

---
+++ log/log.go
@@ -0,0 +1,21 @@
+package log
+
+import (
+	"os"
+
+	"github.com/op/go-logging"
+)
+
+var (
+	Logger   = logging.MustGetLogger("tsl_frame")
+	format = logging.MustStringFormatter(
+		"%{color} %{level:.4s} %{id:03x} %{time:2006-01-02 15:04:05.000} %{shortfile}\t%{shortfunc}\t> %{message}%{color:reset}",
+	)
+)
+
+
+func init() {
+	backend2 := logging.NewLogBackend(os.Stdout, "", 0)
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+	logging.SetBackend(backend2Formatter)
+}
