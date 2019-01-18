77481c11dad00578f3cfe44f517c3e3058eb72a9
dark8605 <3389161@qq.com>
2019-01-18 11:28:45 +0800

    go-xorm封装

---
+++ sql-orm/init.go
@@ -0,0 +1,52 @@
+package sql_orm
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"github.com/go-xorm/xorm"
+)
+
+type Engine struct {
+	Engine *xorm.Engine
+	MaxOpenConns int 	// 最大打开连接数
+	MaxIdleConns int 	// 连接池的空闲数大小
+	Location	 string // 时区
+	State        bool   // 链接状态
+}
+
+var (
+	DataSourceName string
+	DriverName	string
+	EngineCon = Engine{MaxOpenConns: 100, MaxIdleConns: 100, Location: "Asia/Shanghai"}
+	MaxOpenConns int
+	MaxIdleConns int
+	Location  	 string
+	Err			error
+)
+
+func init()  {
+	DataSourceName = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",
+		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"),
+		os.Getenv("DB_POINT"), os.Getenv("DB_NAME"), os.Getenv("DB_CHARSET"))
+	DriverName = os.Getenv("Driver_Name")
+
+	MaxOpenConnsStr := os.Getenv("MaxOpenConns")
+	if MaxOpenConnsStr != "" {
+		if MaxOpenConns,Err = strconv.Atoi(MaxOpenConnsStr); Err == nil {
+			EngineCon.MaxOpenConns = MaxOpenConns
+		}
+	}
+
+	MaxIdleConnsStr := os.Getenv("MaxIdleConns")
+	if MaxIdleConnsStr != "" {
+		if MaxIdleConns,Err = strconv.Atoi(MaxIdleConnsStr); Err == nil {
+			EngineCon.MaxOpenConns = MaxIdleConns
+		}
+	}
+
+	Location = os.Getenv("Location")
+	if Location != "" {
+		EngineCon.Location = Location
+	}
+}
---
+++ sql-orm/orm.go
@@ -0,0 +1,57 @@
+package sql_orm
+
+import (
+	"github.com/go-xorm/xorm"
+	"time"
+	"github.com/Dark86Chen/tsl/log"
+)
+
+
+func (e *Engine)GetOrmEngine() (engine *xorm.Engine, err error) {
+	if EngineCon.Engine != nil {
+		if err := e.Engine.Ping(); err != nil {
+			// 关闭原来的链接
+			e.Engine.Close()
+
+			engine, err := e.createEngine()
+
+			if err != nil {
+				return nil, err
+			}
+			e.Engine = engine
+		}
+	} else {
+		engine, err := e.createEngine()
+
+		if err != nil {
+			return nil, err
+		}
+		e.Engine = engine
+	}
+
+	return e.Engine, nil
+}
+
+
+func (e *Engine)createEngine() (engine *xorm.Engine, err error) {
+	engine, err = xorm.NewEngine(DriverName, DataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	engine.ShowSQL(true)
+	engine.SetMaxOpenConns(10)
+
+	engine.SetMaxOpenConns(e.MaxOpenConns)
+	engine.SetMaxIdleConns(e.MaxIdleConns)
+
+	// 设置时区
+	engine.TZLocation, err = time.LoadLocation(e.Location)
+
+	e.State = true
+
+	if err != nil {
+		log.Logger.Warning("set orm engine location err --> ", err.Error())
+	}
+
+	return engine, nil
+}
