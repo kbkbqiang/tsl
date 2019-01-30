454524792139e8f9cda1e724a4b1171d91d97f31
dark8605 <3389161@qq.com>
2019-01-30 09:51:19 +0800

    10进制转16进制通用方法

--- utils/common.go
+++ utils/common.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"time"
 	"github.com/Dark86Chen/tsl/utils/EAS"
+	"log"
+	"fmt"
 )
 
 // 时间转换 设置时区 东巴区
@@ -66,4 +68,27 @@ func GenerateToken(tokenByte []byte) (token string, err error) {
 		return token, err
 	}
 	return token, nil
+}
+
+// 10进制转16进制
+func DecHex(n int64) string {
+	if n < 0 {
+		log.Println("Decimal to hexadecimal error: the argument must be greater than zero.")
+		return ""
+	}
+	if n == 0 {
+		return "0"
+	}
+	hex := map[int64]int64{10: 65, 11: 66, 12: 67, 13: 68, 14: 69, 15: 70}
+	s := ""
+	for q := n; q > 0; q = q / 16 {
+		m := q % 16
+		if m > 9 && m < 16 {
+			m = hex[m]
+			s = fmt.Sprintf("%v%v", string(m), s)
+			continue
+		}
+		s = fmt.Sprintf("%v%v", m, s)
+	}
+	return s
 }
